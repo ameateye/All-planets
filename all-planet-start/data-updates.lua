09c8f0521829e09d152144dade18d4e36b6621ca
ameateye <amitay.haim.f@gmail.com>
2025-07-25 01:53:39 +0300

    Enabling solar panels and lightning collectors

--- all-planet-start/data-updates.lua
+++ all-planet-start/data-updates.lua
@@ -96,7 +96,9 @@ utils.set_prerequisites("electric-engine", {"lubrication"})
 -- ============================================================================
 
 -- Medium electric poles available with basic electronics
-utils.add_recipes("electronics", {"medium-electric-pole", "iron-stick"})
+utils.add_recipes("electronics", {"medium-electric-pole", "iron-stick","lightning-collector"})
+utils.set_prerequisites("solar-energy", {"steel-processing", "electronics"})
+utils.set_trigger("solar-energy", {type = "craft-item", item = "electronic-circuit", count = 15})
 utils.remove_recipes("electric-energy-distribution-1", {"medium-electric-pole"})
 
 -- ============================================================================
